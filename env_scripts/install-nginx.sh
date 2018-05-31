e79fdbdb38c076c17dda85cffe34bff8f624575b
Huan Jin <huan.jin8@gmail.com>
2018-05-31 12:01:37 +0800

    add/modify nginx/php script

---
+++ env_scripts/install-nginx.sh
@@ -0,0 +1,29 @@
+#!/bin/env sh
+
+if [ -n "$1" ];then
+    TARGET_DIR="$1"
+else
+    TARGET_DIR=`pwd`
+fi
+
+VER="1.14.0"
+NGINX="nginx-$VER"
+NGINX_TAR="$NGINX.tar.gz"
+
+cd $TARGET_DIR
+if [ -f $NGINX_TAR ];then
+    echo "file $NGINX_TAR exists, skip downloading"
+else
+    wget https://nginx.org/download/$NGINX_TAR
+fi
+
+tar xvf $NGINX_TAR
+
+sudo groupadd -r nginx
+sudo useradd -s /sbin/nologin -g nginx -r nginx
+
+sudo yum install -y pcre-devel zlib-devel openssl-devel
+
+cd $NGINX && ./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-http_ssl_module --with-stream --with-stream_ssl_module --user=nginx --group=nginx && make && sudo make install
+
+echo "nginx is under /usr/local/nginx/"
--- env_scripts/install-php7.sh
+++ env_scripts/install-php7.sh
@@ -1,24 +1,39 @@
 #!/bin/env bash
 
 VER=7.2.4
-TAR_FZ=php-src-php-$VER
-TARGET_DIR="$1"
+PHP_FZ=php-src-php-$VER
+PHP_TAR=php-$VER.tar.gz
 
 echo "install deps..."
-sudo yum install epel-release -y
 sudo yum install autoconf libtool re2c bison libxml2-devel bzip2-devel libcurl-devel libpng-devel libicu-devel gcc-c++ libmcrypt-devel libwebp-devel libjpeg-devel openssl-devel -y
 
-echo "download sources"
-if [ -n $TARGET_DIR ];then
-    curl -O -L https://github.com/php/php-src/archive/php-$VER.tar.gz
-    tar -zxvf php-$VER.tar.gz
-    cd php-src-php-$VER
+if [ -n "$1" ];then
+    TARGET_DIR="$1"
+else
+    TARGET_DIR=`pwd`
 fi
+
+cd $TARGET_DIR
+
+if [ -f $PHP_TAR ];then
+    echo "file $PHP_TAR exists, skip download"
+else
+    echo "download $PHP_TAR"
+    curl -O -L https://github.com/php/php-src/archive/php-$VER.tar.gz
+fi
+
+if [ -d $PHP_FZ ];then
+    echo "already extract, skip tar extract"
+else
+    tar -zxvf php-$VER.tar.gz
+fi
+
+cd $PHP_FZ
 
 echo "building from source..."
 ./buildconf --force
-./configure --prefix=/usr/local/php --enable-fpm --disable-short-tags --with-openssl --with-pcre-regex --with-pcre-jit --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-exif --with-gd --enable-intl --enable-mbstring --with-mysqli --enable-pcntl --with-pdo-mysql --enable-soap --enable-sockets --with-xmlrpc --enable-zip --with-webp-dir --with-jpeg-dir --with-png-dir
+./configure --prefix=/usr/local/php7 --enable-fpm --disable-short-tags --with-openssl --with-pcre-regex --with-pcre-jit --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-exif --with-gd --enable-intl --enable-mbstring --with-mysqli --enable-pcntl --with-pdo-mysql --enable-soap --enable-sockets --with-xmlrpc --enable-zip --with-webp-dir --with-jpeg-dir --with-png-dir
 
-make clean
+#make clean
 make
 sudo make install
